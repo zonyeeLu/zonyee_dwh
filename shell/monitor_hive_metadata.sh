487edc8f070edaf63bd38638a95d7dd0c6c57639
xianbing <xianbing>
2019-07-30 09:57:13 +0800

    20190730

--- shell/monitor_hive_metadata.sh
+++ shell/monitor_hive_metadata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mysql_conn=`cat "/tmp/zonyee/mysql_conn/mysql_conn.txt"`
+#mysql_conn=`cat "/tmp/zonyee/mysql_conn/mysql_conn.txt"`
+mysql_conn='mysql -hlocalhost -P3306 -uroot -pkobe24'
 echo $mysql_conn
-sql_str='select type_id,type_name from test.zonyee_test_20190723 into outfile "/tmp/zonyee/zonyee_txt_20190723.txt"'
+#sql_str='select type_id,type_name from test.zonyee_test_20190723 into outfile "/tmp/zonyee/zonyee_txt_20190723.txt"'
+sql_str='select type_id,type_name from test.zonyee_test_20190723'
 echo $sql_str
-echo $sql_str|$mysql_conn
+$mysql_conn -e "$sql_str"
