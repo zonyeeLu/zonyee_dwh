ed94b75d5f6797a4a92c5f3e6fdbcfe1842ac826
zonyee <zonyee_kobe@163.com>
2021-12-21 22:48:10 +0800

    shell 20211221

--- shell/常用命令.sh
+++ shell/常用命令.sh
@@ -46,6 +46,23 @@ free -h  # 查看内容 --human 可读的
 # 9. 查找
 grep -i mem  # 输出包含mem的行 ,忽略大小写 
 
-# 9. 管道操作符 |
+# 10. 管道操作符 |
 free -h|grep -i mem  # 在free -h中查找包含mem的行 -i忽略大小写
 free -h|grep -i mem|awk '{print $4}'  # 在free -h中查找包含mem的行 -i忽略大小写 并且输出第四列  print $0输出整行
+
+# 11. 文件权限
+  # d rwx d表示为目录directory  r表示read  w表示write x表示执行
+
+# 12. 用户权限
+  # 用户分为 所有者(u) 所属组(g) 其他用户(o)
+#   例子 -rw-r--r-- 1 root root  0 xxx   shell.txt 
+#   1. 文件所有者 root 创建者为root 
+#   2. 文件所有者(root用户)对shell.txt 有 rw权限
+#   3. 文件所属组(root组内其他用用户)对shell.txt 有r权限
+#   4. 其他用户(root组以外其他用用户)对shell.txt 有r权限
+
+# 12. 文件权限 chmod
+chmod 777 ./tmp/test.txt # 给test.txt所有用户授rwx的所有权限
+chmod a+x ./tmp/test.txt # 等价上一条命令
+chmod a-x ./tmp/test.txt # 给test.txt删除可执行权限
+chmod g-x,o-x ./tmp/test.txt # 组内用户删除可执行 其他用户删除可执行权限
